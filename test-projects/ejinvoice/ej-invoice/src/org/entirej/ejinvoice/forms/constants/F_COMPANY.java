fc7a2364497774b136f61804116c9109606db823
Paul Harrison <paul.harrison@mojave-i.com>
2014-04-01 17:02:24 +0200

    Various changes including the removal of unwanted class files

--- test-projects/ejinvoice/ej-invoice/src/org/entirej/ejinvoice/forms/constants/F_COMPANY.java
+++ test-projects/ejinvoice/ej-invoice/src/org/entirej/ejinvoice/forms/constants/F_COMPANY.java
@@ -6,78 +6,82 @@ package org.entirej.ejinvoice.forms.constants;
  * entirej plugin from the form.  It
  * should not be modified by hand.
  */
-public class F_COMPANY {
-	public static final String ID = "Company";
+public class F_COMPANY
+{
+    public static final String ID = "Company";
 
-	public static class B_COMPANY_TOOLBAR {
-		public static final String ID = "CompanyToolbar";
-		public static final String I_HOME = "home";
-		public static final String I_NEW = "new";
-		public static final String I_EDIT = "edit";
-		public static final String I_DELETE = "delete";
+    public static class B_COMPANY_TOOLBAR
+    {
+        public static final String ID       = "CompanyToolbar";
+        public static final String I_HOME   = "home";
+        public static final String I_NEW    = "new";
+        public static final String I_EDIT   = "edit";
+        public static final String I_DELETE = "delete";
 
-	}
+    }
 
-	public static class B_COMPANIES {
-		public static final String ID = "Companies";
-		public static final String I_POST_CODE = "postCode";
-		public static final String I_ADDRESS_LINE_2 = "addressLine2";
-		public static final String I_BANK_ADDRESS_LINE_2 = "bankAddressLine2";
-		public static final String I_ACCOUNT_NUMBER = "accountNumber";
-		public static final String I_ADDRESS_LINE_1 = "addressLine1";
-		public static final String I_BANK_ADDRESS_LINE_1 = "bankAddressLine1";
-		public static final String I_BANK_POST_CODE = "bankPostCode";
-		public static final String I_IBAN = "iban";
-		public static final String I_ID = "id";
-		public static final String I_ADDRESS_LINE_3 = "addressLine3";
-		public static final String I_USER_ID = "userId";
-		public static final String I_BANK_TOWN = "bankTown";
-		public static final String I_BANK_COUNTRY = "bankCountry";
-		public static final String I_NAME = "name";
-		public static final String I_TOWN = "town";
-		public static final String I_BANK_NAME = "bankName";
-		public static final String I_VAT_NR = "vatNr";
-		public static final String I_BANK_ADDRESS_LINE_3 = "bankAddressLine3";
-		public static final String I_COUNTRY = "country";
-		public static final String I_COMPANY_INFO_LABEL = "companyInfoLabel";
-		public static final String I_BANK_INFO_LABEL = "bankInfoLabel";
+    public static class B_COMPANIES
+    {
+        public static final String ID                    = "Companies";
+        public static final String I_POST_CODE           = "postCode";
+        public static final String I_ADDRESS_LINE_2      = "addressLine2";
+        public static final String I_BANK_ADDRESS_LINE_2 = "bankAddressLine2";
+        public static final String I_ACCOUNT_NUMBER      = "accountNumber";
+        public static final String I_ADDRESS_LINE_1      = "addressLine1";
+        public static final String I_BANK_ADDRESS_LINE_1 = "bankAddressLine1";
+        public static final String I_BANK_POST_CODE      = "bankPostCode";
+        public static final String I_IBAN                = "iban";
+        public static final String I_ID                  = "id";
+        public static final String I_ADDRESS_LINE_3      = "addressLine3";
+        public static final String I_USER_ID             = "userId";
+        public static final String I_BANK_TOWN           = "bankTown";
+        public static final String I_BANK_COUNTRY        = "bankCountry";
+        public static final String I_NAME                = "name";
+        public static final String I_TOWN                = "town";
+        public static final String I_BANK_NAME           = "bankName";
+        public static final String I_VAT_NR              = "vatNr";
+        public static final String I_BANK_ADDRESS_LINE_3 = "bankAddressLine3";
+        public static final String I_COUNTRY             = "country";
+        public static final String I_COMPANY_INFO_LABEL  = "companyInfoLabel";
+        public static final String I_BANK_INFO_LABEL     = "bankInfoLabel";
 
-	}
+    }
 
-	public static class B_COMPANIES_DETAIL {
-		public static final String ID = "CompaniesDetail";
-		public static final String I_POST_CODE = "postCode";
-		public static final String I_ADDRESS_LINE_2 = "addressLine2";
-		public static final String I_BANK_ADDRESS_LINE_2 = "bankAddressLine2";
-		public static final String I_ACCOUNT_NUMBER = "accountNumber";
-		public static final String I_ADDRESS_LINE_1 = "addressLine1";
-		public static final String I_BANK_ADDRESS_LINE_1 = "bankAddressLine1";
-		public static final String I_BANK_POST_CODE = "bankPostCode";
-		public static final String I_IBAN = "iban";
-		public static final String I_ID = "id";
-		public static final String I_ADDRESS_LINE_3 = "addressLine3";
-		public static final String I_USER_ID = "userId";
-		public static final String I_BANK_TOWN = "bankTown";
-		public static final String I_BANK_COUNTRY = "bankCountry";
-		public static final String I_NAME = "name";
-		public static final String I_TOWN = "town";
-		public static final String I_BANK_NAME = "bankName";
-		public static final String I_VAT_NR = "vatNr";
-		public static final String I_BANK_ADDRESS_LINE_3 = "bankAddressLine3";
-		public static final String I_COUNTRY = "country";
-		public static final String I_COMPANY_INFO_LABEL = "companyInfoLabel";
-		public static final String I_BANK_INFO_LABEL = "bankInfoLabel";
+    public static class B_COMPANIES_DETAIL
+    {
+        public static final String ID                    = "CompaniesDetail";
+        public static final String I_POST_CODE           = "postCode";
+        public static final String I_ADDRESS_LINE_2      = "addressLine2";
+        public static final String I_BANK_ADDRESS_LINE_2 = "bankAddressLine2";
+        public static final String I_ACCOUNT_NUMBER      = "accountNumber";
+        public static final String I_ADDRESS_LINE_1      = "addressLine1";
+        public static final String I_BANK_ADDRESS_LINE_1 = "bankAddressLine1";
+        public static final String I_BANK_POST_CODE      = "bankPostCode";
+        public static final String I_IBAN                = "iban";
+        public static final String I_ID                  = "id";
+        public static final String I_ADDRESS_LINE_3      = "addressLine3";
+        public static final String I_USER_ID             = "userId";
+        public static final String I_BANK_TOWN           = "bankTown";
+        public static final String I_BANK_COUNTRY        = "bankCountry";
+        public static final String I_NAME                = "name";
+        public static final String I_TOWN                = "town";
+        public static final String I_BANK_NAME           = "bankName";
+        public static final String I_VAT_NR              = "vatNr";
+        public static final String I_BANK_ADDRESS_LINE_3 = "bankAddressLine3";
+        public static final String I_COUNTRY             = "country";
+        public static final String I_COMPANY_INFO_LABEL  = "companyInfoLabel";
+        public static final String I_BANK_INFO_LABEL     = "bankInfoLabel";
 
-	}
+    }
 
-	public static final String C_COMPANY_TOOLBAR = "CompanyToolbar";
-	public static final String C_MAIN_SPLIT = "mainSplit";
-	public static final String C_COMPANIES = "Companies";
-	public static final String C_COMPANIES_DETAIL = "CompaniesDetail";
+    public static final String C_COMPANY_TOOLBAR  = "CompanyToolbar";
+    public static final String C_MAIN_SPLIT       = "mainSplit";
+    public static final String C_COMPANIES        = "Companies";
+    public static final String C_COMPANIES_DETAIL = "CompaniesDetail";
 
-	public static final String AC_TOOLBAR_DELETE = "TOOLBAR_DELETE";
-	public static final String AC_TOOLBAR_EDIT = "TOOLBAR_EDIT";
-	public static final String AC_TOOLBAR_HOME = "TOOLBAR_HOME";
-	public static final String AC_TOOLBAR_NEW = "TOOLBAR_NEW";
+    public static final String AC_TOOLBAR_DELETE  = "TOOLBAR_DELETE";
+    public static final String AC_TOOLBAR_EDIT    = "TOOLBAR_EDIT";
+    public static final String AC_TOOLBAR_HOME    = "TOOLBAR_HOME";
+    public static final String AC_TOOLBAR_NEW     = "TOOLBAR_NEW";
 
 }
