7d1113660050f77a5a53f97dd9c645bcf12fa86c
Anuradha Gunasekara <theanuradha@gmail.com>
2014-08-28 23:06:42 +0530

    Added Build By Image

--- test-projects/ejinvoice/ej-invoice/src/org/entirej/ApplicationLauncher.java
+++ test-projects/ejinvoice/ej-invoice/src/org/entirej/ApplicationLauncher.java
@@ -1,6 +1,10 @@
 package org.entirej;
 
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.internal.graphics.ImageFactory;
+import org.entirej.applicationframework.rwt.application.EJRWTImageRetriever;
 import org.entirej.applicationframework.rwt.application.launcher.EJRWTApplicationLauncher;
+import org.entirej.constants.EJ_PROPERTIES;
 import org.entirej.ejinvoice.forms.constants.F_TIME_ENTRY;
 import org.entirej.ejinvoice.forms.constants.F_LOGIN;
 import org.entirej.framework.core.EJFrameworkHelper;
@@ -11,6 +15,7 @@ public class ApplicationLauncher extends EJRWTApplicationLauncher
     @Override
     public void postApplicationBuild(EJFrameworkHelper frameworkHelper)
     {
+        frameworkHelper.setApplicationLevelParameter(EJ_PROPERTIES.P_BUILT_BY, "Built with <a style='color:#' href=\"http://entirej.com/\" target=\"_blank\">"+createEJImageTag()+"</a>");
         frameworkHelper.openForm(F_LOGIN.ID, null, false);
     }
 
@@ -19,6 +24,15 @@ public class ApplicationLauncher extends EJRWTApplicationLauncher
 //        return "resource/theme/default.css";
 //    }
 
+    
+    private String createEJImageTag() {
+        Image image = EJRWTImageRetriever.get("icons/ej_text.png");
+        String imageUrl = ImageFactory.getImagePath(image);
+        return "<img src='" + imageUrl + "' width='55' height='18' style='padding-right:1px;vertical-align:middle;'/>";
+      }
+    
+    
+    
     @Override
     protected String getThemeCSSLocation()
     {
