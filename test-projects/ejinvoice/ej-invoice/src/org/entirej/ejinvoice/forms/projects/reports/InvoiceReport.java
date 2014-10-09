77e417a0933d3b15f758374cb33bd471fed3f717
Anuradha Gunasekara <theanuradha@gmail.com>
2014-10-09 14:32:24 +0530

    Updated INV report to have Bizibo Image

--- test-projects/ejinvoice/ej-invoice/src/org/entirej/ejinvoice/forms/projects/reports/InvoiceReport.java
+++ test-projects/ejinvoice/ej-invoice/src/org/entirej/ejinvoice/forms/projects/reports/InvoiceReport.java
@@ -3,6 +3,9 @@ package org.entirej.ejinvoice.forms.projects.reports;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +14,7 @@ import java.util.Locale;
 
 import net.sf.jasperreports.engine.JRParameter;
 
+import org.entirej.applicationframework.rwt.application.EJRWTImageRetriever;
 import org.entirej.applicationframework.rwt.file.EJRWTFileDownload;
 import org.entirej.framework.core.EJManagedFrameworkConnection;
 import org.entirej.reports.EJReportExportType;
@@ -27,8 +31,24 @@ public class InvoiceReport
             File tempFile = File.createTempFile("tmpejinv", String.valueOf(invId));
 
             EJReportParameter invID = new EJReportParameter("EJ_INV_ID", invId);
+            EJReportParameter invBiziboImage = new EJReportParameter("EJ_BIZIBO_IMG", null);
+
+            // only add Image value if need to show in report
+            URL img = InvoiceReport.class.getClassLoader().getResource("icons/Bizibo.png");
+            if (img != null)
+            {
+                try
+                {
+                    invBiziboImage.setValue(Files.readAllBytes(Paths.get(img.toURI())));
+                }
+                catch (URISyntaxException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
             EJReportParameter localeParameter = new EJReportParameter(JRParameter.REPORT_LOCALE, locale);
-            EJReports.exportReport(InvoiceReport.class.getResourceAsStream("INV_A4.jasper"), tempFile.getAbsolutePath(), EJReportExportType.PDF, (Connection) connection.getConnectionObject(), invID, localeParameter);
+            EJReports.exportReport(InvoiceReport.class.getResourceAsStream("INV_A4.jasper"), tempFile.getAbsolutePath(), EJReportExportType.PDF, (Connection) connection.getConnectionObject(), invID, invBiziboImage, localeParameter);
             EJRWTFileDownload.download(tempFile.getAbsolutePath(), exportName + ".pdf");
             tempFile.deleteOnExit();
         }
@@ -45,8 +65,24 @@ public class InvoiceReport
             File tempFile = File.createTempFile("tmpejinv", String.valueOf(invId));
 
             EJReportParameter invID = new EJReportParameter("EJ_INV_ID", invId);
+
+            EJReportParameter invBiziboImage = new EJReportParameter("EJ_BIZIBO_IMG", null);
+
+            // only add Image value if need to show in report
+            URL img = InvoiceReport.class.getClassLoader().getResource("icons/Bizibo.png");
+            if (img != null)
+            {
+                try
+                {
+                    invBiziboImage.setValue(Files.readAllBytes(Paths.get(img.toURI())));
+                }
+                catch (URISyntaxException e)
+                {
+                    e.printStackTrace();
+                }
+            }
             EJReportParameter localeParameter = new EJReportParameter(JRParameter.REPORT_LOCALE, locale);
-            EJReports.exportReport(InvoiceReport.class.getResourceAsStream("INV_A4.jasper"), tempFile.getAbsolutePath(), EJReportExportType.PDF, (Connection) connection.getConnectionObject(), invID, localeParameter);
+            EJReports.exportReport(InvoiceReport.class.getResourceAsStream("INV_A4.jasper"), tempFile.getAbsolutePath(), EJReportExportType.PDF, (Connection) connection.getConnectionObject(), invID, invBiziboImage,localeParameter);
 
             Path path = Paths.get(tempFile.getAbsolutePath());
             byte[] data = Files.readAllBytes(path);
